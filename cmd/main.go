06953fef2b3b907344d8112f730c37a0af747d54
webstradev <e.s.westra.95@gmail.com>
2023-06-27 12:17:25 +0200

    remove uneccessary main code

--- cmd/main.go
+++ cmd/main.go
@@ -1,33 +1,7 @@
 package main
 
-import (
-	"log"
-
-	"github.com/westradev/webbr/webbr"
-)
+import "fmt"
 
 func main() {
-	userData := webbr.M{
-		"name": "Erik",
-		"age":  "27",
-	}
-
-	wb, err := webbr.New()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	id, err := wb.Insert("users", userData)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Inserted user with id: ", id)
-
-	results, err := wb.Find("users", webbr.Filter{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Printf("%+v\n", results)
+	fmt.Println("Webstra was here!")
 }
