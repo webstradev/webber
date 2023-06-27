2095de85f0cef7423571e9f359fa3dbf76a4222a
webstradev <e.s.westra.95@gmail.com>
2023-06-27 13:08:46 +0200

    WIP check-in basic API stuff

---
+++ api/api.go
@@ -0,0 +1,42 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/westradev/webbr/webbr"
+)
+
+type Server struct {
+	db *webbr.Webbr
+}
+
+func NewServer(db *webbr.Webbr) *Server {
+	return &Server{
+		db: db,
+	}
+}
+
+func (s *Server) HandlePostInsert(c echo.Context) error {
+	var (
+		collName = c.Param("collname")
+	)
+
+	var data webbr.M
+	if err := json.NewDecoder(c.Request().Body).Decode(&data); err != nil {
+		return err
+	}
+
+	id, err := s.db.Insert(collName, data)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusCreated, webbr.M{"id": id})
+}
+
+func (s *Server) HandleGetQuery(c echo.Context) error {
+
+	return nil
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/labstack/echo/v4"
+	"github.com/westradev/webbr/api"
+	"github.com/westradev/webbr/webbr"
+)
 
 func main() {
-	fmt.Println("Webstra was here!")
+	db, err := webbr.New(webbr.WithDBName("main"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := api.NewServer(db)
+
+	e := echo.New()
+	e.HideBanner = true
+
+	e.POST("/:dbname/:collname", server.HandlePostInsert)
+	e.GET("/:dbname/:collname", server.HandleGetQuery)
+	log.Fatal(e.Start(":6969"))
+
 }
